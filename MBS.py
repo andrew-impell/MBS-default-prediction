75d597e8aba257bd35d411d1fc4e0a860a7877b2
andrew-impell <andrew-impell@users.noreply.github.com>
2020-05-19 02:45:15 -0400

    Added SGDClassifier()
    Moved routines to functions so that we could add iterative functionality for multiple quarters

--- MBS.py
+++ MBS.py
@@ -1,3 +1,4 @@
+import gc
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -5,8 +6,9 @@ import numpy as np
 import os
 from collections import Counter
 import glob
+from sklearn.calibration import CalibratedClassifierCV
 from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
@@ -37,90 +39,117 @@ init_cols = ['Credit Score', 'First Payment Date', 'First Timebuyer',
              'Pre-HARP loan seq']
 
 time_cols = ['Loan Sequence', 'Monthly Report Per', 'Curr UPB',
-             'Current Loan Del', 'Loan Age', 'Remaining Months Mat', 'Repo Flag',
-             'Modi Flag', 'Zero bal', 'Zero balance eff', 'Current IR', 'Current deferred UPB',
-             'Due Date of Last Paid Installment', 'MI Recoveries', 'Net Sale Pro',
-             'Non MI recoveries', 'Expenses', 'Legal Cost', 'Maint', 'Taxes Insur',
-             'Misc', 'Actual Loss', 'Mod Cost', 'Step Mod Flag', 'Def Pay Mod',
-             'ELTV']
+             'Current Loan Del', 'Loan Age', 'Remaining Months Mat',
+             'Repo Flag', 'Modi Flag', 'Zero bal', 'Zero balance eff',
+             'Current IR', 'Current deferred UPB',
+             'Due Date of Last Paid Installment', 'MI Recoveries',
+             'Net Sale Pro', 'Non MI recoveries', 'Expenses', 'Legal Cost',
+             'Maint', 'Taxes Insur', 'Misc', 'Actual Loss', 'Mod Cost',
+             'Step Mod Flag', 'Def Pay Mod', 'ELTV']
 
-init_str = 'historical_data1_Q[1-2]20?8.txt'
-time_str = 'historical_data1_time_Q[1-2]20?8.txt'
+init_str = 'historical_data1_Q[1]20?8.txt'
+time_str = 'historical_data1_time_Q[1]20?8.txt'
 
 init_data = list(glob.glob(str(cwd) + '/data/' + init_str))
 time_data = list(glob.glob(str(cwd) + '/data/' + time_str))
 
-init_list = []
-time_list = []
 
-for fil in init_data:
-    print(f'Reading {fil}..')
-    df = pd.read_csv(fil, sep='|', names=init_cols)
-    init_list.append(df)
+def read_data(init_id, time_id):
 
-for fil in time_data:
-    print(f'Reading {fil}..')
-    df = pd.read_csv(fil, sep='|', names=time_cols)
-    time_list.append(df)
+    # Read in data from a specific quarter then output the train test data
+    # init_id: file location of the initialization data
+    # time_id: file location of the loan performance data
 
-init_big = pd.concat(init_list, axis=0)
-time_big = pd.concat(time_list, axis=0)
+    print(f'Reading {init_id}..')
+    init_big = pd.read_csv(init_id, sep='|', names=init_cols)
 
-init_big.sort_values(by='Loan Sequence')
-time_big.sort_values(by='Loan Sequence')
+    print(f'Reading {time_id}..')
+    time_big = pd.read_csv(time_id, sep='|', names=time_cols)
 
+    init_big.sort_values(by='Loan Sequence')
+    time_big.sort_values(by='Loan Sequence')
 
-delinq = time_big['Current Loan Del']
-print(delinq.unique)
-print(init_big.shape[0])
+    delinq = time_big['Current Loan Del']
+    print(delinq.unique)
+    print(init_big.shape[0])
 
-features = ['Credit Score', 'First Payment Date', 'First Timebuyer',
-            'Maturity Date', 'MSA', 'MI %', 'Original IR', 'UPB']
+    features = ['Credit Score', 'First Payment Date', 'First Timebuyer',
+                'Maturity Date', 'MSA', 'MI %', 'Original IR', 'UPB']
 
-init_big = init_big[features]
+    init_big = init_big[features]
 
-init_big['TARGET'] = delinq.map(get_default)
-init_big['First Timebuyer'] = init_big['First Timebuyer'].map(first_time_transform)
-init_big.fillna(0, inplace=True)
+    init_big['TARGET'] = delinq.map(get_default)
+    init_big['First Timebuyer'] = \
+        init_big['First Timebuyer'].map(first_time_transform)
+    init_big.fillna(0, inplace=True)
 
-feat = init_big.loc[:, init_big.columns != 'TARGET']
-target = init_big.TARGET
-class_names = init_big.TARGET
+    feat = init_big.loc[:, init_big.columns != 'TARGET']
+    target = init_big.TARGET
 
+    features_train, features_test, target_train, target_test = \
+        train_test_split(feat, target, test_size=0.20, random_state=10)
 
-features_train, features_test, target_train, target_test = train_test_split(
-    feat, target, test_size=0.20, random_state=10)
+    return features_train, features_test, target_train, target_test
 
 
-print(features_train.shape, features_test.shape, target_train.shape, target_test.shape)
+def train_model(features_train, features_test, target_train, target_test):
 
-clf = GaussianNB()
-clf.fit(features_train, target_train)
-target_pred = clf.predict(features_test)
-predicted_probas = clf.predict_proba(features_test)
+    # takes in data and trains the inital quarter data
+    clf = SGDClassifier()
+    clf.fit(features_train, target_train)
+    calibrator = CalibratedClassifierCV(clf, cv='prefit')
+    model = calibrator.fit(features_train, target_train)
 
-acc = accuracy_score(target_test, target_pred)
+    target_pred = clf.predict(features_test)
+    predicted_probas = model.predict_proba(features_test)
 
-cnf_matrix = confusion_matrix(target_test, target_pred)
+    acc = accuracy_score(target_test, target_pred)
 
-
-ax = plt.subplot()
-sns.heatmap(cnf_matrix, annot=True, cmap=plt.cm.Blues,
-            ax=ax, fmt='g')  # annot=True to annotate cells
-
-# labels, title and ticks
-ax.set_xlabel('Predicted labels')
-ax.set_ylabel('True labels')
-ax.set_title('Confusion Matrix')
-ax.xaxis.set_ticklabels(['0', '1'])
-ax.yaxis.set_ticklabels(['0', '1'])
-
-plt.show()
+    cnf_matrix = confusion_matrix(target_test, target_pred)
+    return clf, cnf_matrix, acc
 
 
-logreg = LogisticRegression(C=0.0001)
-logreg.fit(features_train, target_train)
-target_pred = logreg.predict(features_test)
-predicted_probas = logreg.predict_proba(features_test)
+clf, cnf_matrix, acc = \
+    train_model(features_train, features_test, target_train, target_test)
 
-print(accuracy_score(target_test, target_pred))
+# Delete the dataframe before getting new data and updating the clf
+
+del init_big, time_big
+del features_train, features_test, target_train, target_test
+gc.collect()
+
+
+def update_model(
+        features_train, features_test, target_train, target_test, clf):
+    # takes in new quarter data and a clf and outputs new trained metrics/clf
+
+    clf.partial_fit(features_train, target_train)
+    calibrator = CalibratedClassifierCV(clf, cv='prefit')
+    model = calibrator.fit(features_train, target_train)
+
+    target_pred = clf.predict(features_test)
+    predicted_probas = model.predict_proba(features_test)
+    acc = accuracy_score(target_test, target_pred)
+
+    cnf_matrix = confusion_matrix(target_test, target_pred)
+    return clf, cnf_matrix, acc
+
+
+clf, cnf_matrix, acc = \
+    update_model(features_train, features_test, target_train, target_test, clf)
+
+
+def plot_data(cnf_matrix):
+    ax = plt.subplot()
+    sns.heatmap(cnf_matrix, annot=True, cmap=plt.cm.Blues,
+                ax=ax, fmt='g')  # annot=True to annotate cells
+
+    # labels, title and ticks
+    ax.set_xlabel('Predicted labels')
+    ax.set_ylabel('True labels')
+    ax.set_title('Confusion Matrix')
+    ax.xaxis.set_ticklabels(['0', '1'])
+    ax.yaxis.set_ticklabels(['0', '1'])
+
+    plt.show()
+    return 0
